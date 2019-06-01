584d6ca7cc6a3ff6cc599a658d57fe0bbee29a59
root <root@pop-os.localdomain>
2019-06-01 12:16:51 +0530

    Basic initialization of fb footer link plugin

---
+++ fb-link-footer.php
@@ -0,0 +1,12 @@
+<?php
+
+ /*
+    Plugin Name: Facebook Footer Link
+    Description: Adds Facebook Profile Link in Footer
+    Version: 0.0.1
+    Author: VOMAkSh
+ */
+
+ if (!defined(ABSPATH)) {
+     exit ;
+ }
